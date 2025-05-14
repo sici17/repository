d89d468df487afa4119a29541afe7600fb9256b8
sici17 <francescosiciliani624@gmail.com>
2025-05-14 17:10:35 +0200

    Update Libreria.java
    general fixes

--- src/libreria/Libreria.java
+++ src/libreria/Libreria.java
@@ -5,13 +5,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-package libreria;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
